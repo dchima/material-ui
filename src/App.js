6d1bb6f1a6e8cfae89d1fb115c5fef3935327b96
dchima <dcnnad@gmail.com>
2020-12-01 08:52:08 +0100

    :cyclone: testing icons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core'
+import { Save } from '@material-ui/icons'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button 
+          startIcon={<Save />}
+          variant='contained'
+          color='primary'>
+          Desolation
+        </Button>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
